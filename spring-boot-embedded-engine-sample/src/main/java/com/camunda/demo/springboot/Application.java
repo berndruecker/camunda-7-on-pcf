a4ec0a78ee89b173f2278c45cd69f007ba84f531
Bernd Rücker <bernd.ruecker@camunda.com>
2019-03-12 14:00:02 +0100

    use @ProcessApplication for embedded engine

--- spring-boot-embedded-engine-sample/src/main/java/com/camunda/demo/springboot/Application.java
+++ spring-boot-embedded-engine-sample/src/main/java/com/camunda/demo/springboot/Application.java
@@ -1,9 +1,11 @@
 package com.camunda.demo.springboot;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class Application {
 
   public static void main(String... args) {
