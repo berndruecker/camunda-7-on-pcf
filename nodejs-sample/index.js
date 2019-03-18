605732b42e6be2e7b997a44dd026c405ca5f7b10
Bernd Rücker <bernd.ruecker@camunda.com>
2019-03-18 16:22:12 +0100

    switched from camunda-bpm-sdk to plain REST

--- nodejs-sample/index.js
+++ nodejs-sample/index.js
@@ -1,89 +1,90 @@
-var camundaEngineUrl = "http://localhost:8080/rest/"; // default if not overwritten by ENV variable
-var targetPort = "8090";
+var request = require('request');
+var {Client} = require('camunda-external-task-client-js');
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
 
+
+var camundaEngineUrl = 'http://localhost:8080/rest/'; // default if not overwritten by ENV variable
+var targetPort = '8090'; //default if not overwritten by ENV
 if (process.env.CAMUNDA_URL) {
   camundaEngineUrl = process.env.CAMUNDA_URL;
 }
 if (process.env.TARGET_PORT) {
   targetPort = process.env.TARGET_PORT;
 }
-console.log("Use Camunda Server at " + camundaEngineUrl);
+console.log('Use Camunda Server at ' + camundaEngineUrl);
 
-var fs = require('fs');
-var path = require('path');
 
 // setup external task client
-const {Client} = require("camunda-external-task-client-js");
-const client = new Client( { baseUrl: camundaEngineUrl, interval: 50, asyncResponseTimeout: 10000});
-
-// setup Camunda client
-var CamundaSDK = require('camunda-bpm-sdk-js');
-var camundaClient = new CamundaSDK.Client({ mock: false, apiUri: camundaEngineUrl });
-var processDefinitionService  = new camundaClient.resource('process-definition');
-var deploymentService         = new camundaClient.resource('deployment');
-
+var client = new Client( { baseUrl: camundaEngineUrl, interval: 50, asyncResponseTimeout: 10000});
 // External Task subscription to do business logic
-client.subscribe("sysout", async function({ task, taskService }) {
-  console.log('Hello World: %s', task.variables.get("text"));
+client.subscribe('sysout', async function({ task, taskService }) {
+  console.log('Hello World: %s', task.variables.get('text'));
   await taskService.complete(task);
 });
 
+
 // Deployment of Workflow Definition during startup (duplicates are NOT deployed)
 function deployProcess() {
-    filename = "sysout.bpmn";
+    filename = 'sysout.bpmn';
     path = path.join(__dirname, filename);
     console.log(path);
 
-    fs.readFile(path, function (err, content) {
-      if (err) { throw err }
+    request(
+        {
+          method: "POST", // see https://docs.camunda.org/manual/latest/reference/rest/deployment/post-deployment/
+          uri: camundaEngineUrl + 'engine/default/deployment/create',
+          headers: {
+              "Content-Type": "multipart/form-data"
+          },
+          formData : {
+              'deployment-name': 'sysout',
+              'enable-duplicate-filtering': 'true',
+              'deploy-changed-only': 'true',
+              'scripttest.bpmn': {
+                'value':  fs.createReadStream(path),
+                'options': {'filename': filename}
+              }
+          }
+        }, function (err, res, body) {
+            if (err) {
+              console.log(err);
+              throw err;
+            }
+            console.log('[%s] Deployment succeeded: ' + body);
+    
+    });
+}
 
-      console.log(content.toString());
-      
-      // create a deployment with...
-      deploymentService.create({
-        // ... the settings
-        deploymentName:           "sysout",
-        enableDuplicateFiltering: "true",
-        deployChangedOnly:        "true",
-        files:                    [ {
-          name:    filename,
-          content: content.toString()
-        } ]
-      }, function (err, deployment) {
+// Start workflow instance: https://docs.camunda.org/manual/latest/reference/rest/process-definition/post-start-process-instance/
+function startProcess(text) {
+  request(
+    {
+      method: "POST", // see https://docs.camunda.org/manual/latest/reference/rest/deployment/post-deployment/
+      uri: camundaEngineUrl + 'engine/default/process-definition/key/'+'sysout'+'/start',
+      json: {
+        'variables': {
+          'text' : {
+              'value' : text,
+              'type': 'String'
+          },
+        }      
+      }
+    }, function (err, res, body) {
         if (err) {
           console.log(err);
           throw err;
         }
-        console.log('[%s] Deployment "' + deployment.name + '" succeeded, ' + deployment.deploymentTime);
-      });
-    });
-
-}
-
-// Auto deploy on Startup
-deployProcess();
-
-// Start workflow instance
-function startProcess(text) {
-  processDefinitionService.submit({
-        key: "sysout",
-        variables: {
-          "text" : {
-              "value" : text,
-              "type": "String"
-          }
-        }
-  }, function (err) {
-    if (err) {
-      throw err;
-    }
-
-    console.log('Process instance started: ' + text);
+        console.log('Process instance started: ' + body);
   });
 }
 
+// Startup: Auto-Deploy
+deployProcess();
+
+
 // Webserver to provide REST API to start workflow instances
-var express = require('express');
 var app = express();
 app.use(express.json());
 
@@ -95,5 +96,5 @@ app.post('/hello', function (req, res) {
 var server = app.listen(targetPort, function () {
   var host = server.address().address
   var port = server.address().port
-  console.log("REST API now listening at http://%s:%s", host, port)
+  console.log('REST API now listening at http://%s:%s', host, port)
 })
